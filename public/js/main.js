a87c9372b65f80cd61e930d1f078b8d097748116
DeepeshKalura <deepeshkalurs@gmail.com>
2024-07-28 18:20:20 +0530

    chore: Update viewport meta tag in index.html

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    var menu = document.getElementById('menu');
+    console.log("Hello world");
+    menu.classList.toggle('hidden');
+});
