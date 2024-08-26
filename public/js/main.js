49d47f3fa509b625cd15793b0f627ef976bd0b9d
DeepeshKalura <deepeshkalurs@gmail.com>
2024-08-26 11:58:55 +0530

    fix: issue
    1. Clickbality of menu in mobile
    2. Marigin error in booking section
    3. indexing logic error

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
+let toggleButtoned = document.getElementById('menu-toggle');
+toggleButtoned.addEventListener('click', function () {
     var menu = document.getElementById('menu');
     console.log("Hello world");
     menu.classList.toggle('hidden');
 });
+
